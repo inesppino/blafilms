5aeea191c414149882c4e7da90eb31481bbcfe2f
Ines Pedraza <ines.pedraza@ultimasoftware.com>
2021-02-01 17:13:20 +0100

    feat(App): paginación funciona correctamente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import './App.css'
 import placeholderImg from './placeholder.png'
 import { ReactComponent as ChevronLeft } from './chevron-left.svg'
@@ -7,47 +7,67 @@ import InputSearch from './components/input-search/InputSearch'
 
 function App() {
   const [list, setList] = useState([]);
+  const [query, setQuery] = useState('');
+  const [numberPage, setNumberPage] = useState(1);
+  const [finalNumberOfPages, setFinalNumberOfPages] = useState(1);
+  const maxPagesAllowed = 100;
+  const pageSize = 10;
 
-  useEffect(() => {
-    const search = async () => {
-      const response = await fetch(
-        'http://www.omdbapi.com/?apikey=a461e386&s=king',
-      )
-      const data = await response.json()
-      setList(data);
-    };
+  const saveQuery = (query) => {
+    setQuery(query);
+  };
 
-    search();
-  },[]);
-
-  const getMovies = async (query) => {
+  const getMovies = useCallback(async () => {
     try {
       const response = await fetch(
-        `http://www.omdbapi.com/?apikey=a461e386&s=${query}`,
+        `http://www.omdbapi.com/?apikey=a461e386&page=${numberPage}&s=${query}`,
       );
       const data = await response.json();
-      console.log({data})
       if(data.Search && data.Search.length) {
         setList([]);
         setList(data);
+        setNumberOfTotalPages(data.totalResults);
       } else {
         setList([]);
+        setNumberOfTotalPages(1);
       }
     }
     catch (ex){
       alert(ex)
     }
+  },[query, numberPage]);
+
+  const setNumberOfTotalPages = (totalItems) => {
+    let pages = 0;
+    if(totalItems > (maxPagesAllowed*pageSize)) {
+      pages = 100
+    } else {
+      pages = Math.ceil(totalItems / pageSize)
+    }
+    setFinalNumberOfPages(pages);
   };
+
+  const onChangePageClicked = (way) => {
+    if(way === 'next' && numberPage < finalNumberOfPages) {
+      setNumberPage(numberPage + 1);
+    } else if(way === 'prev' && numberPage !== 1) {
+      setNumberPage(numberPage - 1);
+    }
+  };
+
+  useEffect(() =>{
+    getMovies();
+  },[getMovies]);
 
   return (
     <div className="App">
-      <InputSearch searchAction={getMovies}/>
+      <InputSearch searchAction={saveQuery}/>
       {list.length === 0? (
         <p>No results yet</p>
       ) : (
         <div className="search-results">
           <div className="chevron">
-            <ChevronLeft />
+            {numberPage !== 1 && <ChevronLeft onClick={() => onChangePageClicked('prev')}/>}
           </div>
           <div className="search-results-list">
             {list.Search.map(result => (
@@ -64,7 +84,7 @@ function App() {
             ))}
           </div>
           <div className="chevron">
-            <ChevronRight />
+            {numberPage !== finalNumberOfPages && <ChevronRight onClick={() => onChangePageClicked('next')}/>}
           </div>
         </div>
       )}
